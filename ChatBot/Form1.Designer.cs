c00916dde396c9df49823164f8038bcb0c0b0601
Derek Riddle <derekmriddle@gmail.com>
2017-05-07 20:15:03 -0500

    Adding Project

---
+++ ChatBot/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ChatBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChat = new System.Windows.Forms.Button();
+            this.richChat = new System.Windows.Forms.RichTextBox();
+            this.txtChatBox = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtChatRoom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtOauth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChat
+            // 
+            this.btnChat.Location = new System.Drawing.Point(12, 423);
+            this.btnChat.Name = "btnChat";
+            this.btnChat.Size = new System.Drawing.Size(383, 48);
+            this.btnChat.TabIndex = 0;
+            this.btnChat.Text = "Chat";
+            this.btnChat.UseVisualStyleBackColor = true;
+            this.btnChat.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richChat
+            // 
+            this.richChat.Location = new System.Drawing.Point(12, 12);
+            this.richChat.Name = "richChat";
+            this.richChat.ReadOnly = true;
+            this.richChat.Size = new System.Drawing.Size(383, 379);
+            this.richChat.TabIndex = 1;
+            this.richChat.Text = "";
+            // 
+            // txtChatBox
+            // 
+            this.txtChatBox.Location = new System.Drawing.Point(12, 397);
+            this.txtChatBox.Name = "txtChatBox";
+            this.txtChatBox.Size = new System.Drawing.Size(383, 20);
+            this.txtChatBox.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 477);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(182, 43);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(200, 477);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(195, 43);
+            this.btnDisconnect.TabIndex = 4;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtChatRoom
+            // 
+            this.txtChatRoom.Location = new System.Drawing.Point(12, 526);
+            this.txtChatRoom.Name = "txtChatRoom";
+            this.txtChatRoom.Size = new System.Drawing.Size(100, 20);
+            this.txtChatRoom.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 529);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "<== Chatroom to join";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(320, 719);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 552);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 9;
+            // 
+            // txtOauth
+            // 
+            this.txtOauth.Location = new System.Drawing.Point(12, 578);
+            this.txtOauth.Name = "txtOauth";
+            this.txtOauth.Size = new System.Drawing.Size(100, 20);
+            this.txtOauth.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 555);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "<== Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 581);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "<== Oauth";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(408, 754);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOauth);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtChatRoom);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtChatBox);
+            this.Controls.Add(this.richChat);
+            this.Controls.Add(this.btnChat);
+            this.Name = "Form1";
+            this.Text = "Dirk\'s Chat Bot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChat;
+        private System.Windows.Forms.RichTextBox richChat;
+        private System.Windows.Forms.TextBox txtChatBox;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtChatRoom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtOauth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChatBot/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using TwitchLib;
+using TwitchLib.Events.Client;
+using TwitchLib.Enums;
+
+namespace ChatBot
+{
+    public partial class Form1 : Form
+    {
+        //makes the main TwitchClient object
+        TwitchClient client = new TwitchClient(new TwitchLib.Models.Client.ConnectionCredentials(Properties.Settings.Default.username, Properties.Settings.Default.oauth));
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                //puts txt box into chat box
+                richChat.Text += Properties.Settings.Default.username + ": " + txtChatBox.Text + "\n";
+                //sends message to twitch chat server
+                client.SendMessage(txtChatBox.Text);
+
+                txtChatBox.Clear();
+            } else
+            {
+                richChat.Text += "Disconnected from chat!\n";
+            }
+            
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            client.OnMessageReceived += new EventHandler<OnMessageReceivedArgs>(globalChatMessagesRecieved);
+            client.OnConnected += new EventHandler<OnConnectedArgs>(onConnected);
+            client.OnDisconnected += new EventHandler<OnDisconnectedArgs>(onDisconnected);
+
+            if (client.IsConnected == false)
+            {
+                client.Connect();
+            }
+        }
+
+        public void onConnected(object sender, OnConnectedArgs e)
+        {
+            //Figure out why we need this
+            CheckForIllegalCrossThreadCalls = false;
+
+            //joins chatroom
+            client.JoinChannel(txtChatRoom.Text);
+
+            richChat.Text += "Connected to chat server!\n";
+        }
+
+        public void onDisconnected(object sender, OnDisconnectedArgs e)
+        {
+            //Figure out why we need this
+            CheckForIllegalCrossThreadCalls = false;
+            richChat.Text += "Disconnected from channel.";
+        }
+
+        private void globalChatMessagesRecieved(object sender, OnMessageReceivedArgs e)
+        {
+            //Figure out why we need this
+            CheckForIllegalCrossThreadCalls = false;
+
+            if (e.ChatMessage.Username == "dirkadin" & e.ChatMessage.Message.StartsWith("hello"))
+            {
+                //soundPlayer.Load();
+                //soundPlayer.Play();
+                System.Media.SystemSounds.Question.Play();
+
+                updateChat(e);
+            }
+            else if (e.ChatMessage.UserType == UserType.Moderator & e.ChatMessage.Message == "!foo")
+            {
+                System.Media.SystemSounds.Question.Play();
+                updateChat(e);
+            }
+            else
+            {
+                updateChat(e);
+            }
+            
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            client.LeaveChannel(txtChatRoom.Text);
+            client.Disconnect();
+            richChat.Text += "Disconnecting...\n";
+        }
+
+        private void updateChat(OnMessageReceivedArgs e)
+        {
+            richChat.AppendText(e.ChatMessage.Username + ": " + e.ChatMessage.Message + "\n");
+            richChat.ScrollToCaret();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            client.LeaveChannel(txtChatRoom.Text);
+            client.Disconnect();
+            richChat.Text += "Disconnecting...\n";
+            Close();
+        }
+    }
+}
